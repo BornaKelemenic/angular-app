3495fc8d2b1e6cb8d8f4dfff678e07371c95abb6
BornaKelemenic <34454621+BornaKelemenic@users.noreply.github.com>
2017-12-14 15:12:45 +0100

    datatables integration

--- client/src/app/components/contacts/contacts-table/contacts-table.component.ts
+++ client/src/app/components/contacts/contacts-table/contacts-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../../../services/message.service';
 import { ContactsService } from '../../../services/contacts.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-contacts-table',
@@ -10,6 +11,8 @@ import { ContactsService } from '../../../services/contacts.service';
 export class ContactsTableComponent implements OnInit
 {
   contacts;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
 
   constructor(
     public msgService: MessageService,
@@ -18,6 +21,29 @@ export class ContactsTableComponent implements OnInit
   {}
 
   ngOnInit()
+  {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      rowCallback: (row: Node, data: any[] | Object, index: number) => 
+      {
+        const self = this;
+        // Unbind first in order to avoid any duplicate handler
+        $('td', row).unbind('click');
+        $('td', row).bind('click', () => {
+          // Click handler
+        });
+        return row;
+      }
+    };
+
+    this.getContacts();
+  }
+
+  /**
+   * Get all contacts
+   */
+  private getContacts()
   {
     this.contactService.getContacts().subscribe(res => 
     {
@@ -29,6 +55,7 @@ export class ContactsTableComponent implements OnInit
       {
         this.msgService.createSuccessMessage(res.msg);
         this.contacts = res.contacts;
+        this.dtTrigger.next();
 
         setTimeout(() => {
           this.msgService.removeInfoMessages();
